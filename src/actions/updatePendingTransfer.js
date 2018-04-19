57248f50378e282ce0e4acfc0f1a3a5e5bc21ffa
SJ Pratt <hey@pratt.jp>
2018-04-19 13:58:23 -0500

    fix tx input extraction

--- src/actions/updatePendingTransfer.js
+++ src/actions/updatePendingTransfer.js
@@ -1,6 +1,6 @@
 import notifyAccountTransferUpated from './notifyAccountTransferUpdated'
 import updateTransfer from '../actions/updateTransfer'
-import {getTransaction, getTransactionReceipt} from '../services/contractService'
+import {getTransaction, getTransactionReceipt, CKContractABI} from '../services/contractService'
 import {STATUS_PENDING, statusForTransaction, getTransferInputs} from '../utils'
 
 export default (async function updatePendingTransfer(transfer) {
@@ -18,7 +18,7 @@ export default (async function updatePendingTransfer(transfer) {
   }
   let currentStatus = transfer.status
   if (tx) {
-    const transferInputs = getTransferInputs(tx, txReceipt) || {}
+    const transferInputs = getTransferInputs(CKContractABI, tx) || {}
     currentStatus = statusForTransaction(tx, txReceipt)
     updateValues.from = tx.from
     updateValues.to = transferInputs.to
